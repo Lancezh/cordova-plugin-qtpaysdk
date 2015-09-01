d95172274dda6259da3478ac6bbef388c35af939
Lancezh <zjllance@foxmail.com>
2015-09-01 15:51:58 +0800

    add possdk orderPayByPos

--- src/android/QTPaySDK.java
+++ src/android/QTPaySDK.java
@@ -1,4 +1,4 @@
-package com.qfpay.qpossdk.QTPaySDK;
+package com.qfpay.QTPaySDK;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -9,33 +9,76 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.qfpay.qpossdk.entity.PayOrderInfo;
+import com.qfpay.qpossdk.pay.QfPayCallBack;
+import com.qfpay.qpossdk.pay.QfPaySdk;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class QTPaySDK extends CordovaPlugin {
 
+    private CallbackContext callbackContext;
+    private Activity activity = null;
+
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         // your init code here
-        mqt = QTPayCommon.getInstance(getApplicationContext());
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
+        this.callbackContext = callbackContext;
+        this.activity = this.cordova.getActivity();
+        boolean retValue = true;
+
+        if (action.equals("orderPayByPos")) {
+            this.orderPayByPos(args);
+        } else {
+            retValue = false;
         }
-        return false;
+
+        return retValue;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    QfPayCallBack payCallBack = new QfPayCallBack() {
+        @Override
+        public void onPayResultFail() {
+            this.callbackContext.error("fail");
+//            queryOrder();
         }
+
+        @Override
+        public void onPayResultSuccess() {
+            this.callbackContext.success("success");
+//            queryOrder();
+        }
+
+        @Override
+        public void onPayResultCancel() {
+            this.callbackContext.error("cancel");
+//            queryOrder();
+        }
+
+        @Override
+        public void onPayResultUnknow() {
+            this.callbackContext.error("unknow");
+//            queryOrder();
+        }
+    };
+
+    private void orderPayByPos(JSONArray args) {
+        JSONObject jObject = args.getJSONObject(0);
+
+        PayOrderInfo orderInfo = new PayOrderInfo();
+        orderInfo.createUserId = jObject.getString("createUserId");
+        orderInfo.payOrderId = jObject.getString("payOrderId");
+        orderInfo.qfToken = jObject.getString("qfToken");
+        orderInfo.token = jObject.getString("token");
+        orderInfo.platform = jObject.getString("platform");
+        orderInfo.mobile = jObject.getString("mobile");
+
+        QfPaySdk.getInstance().payOrder(this.activity, orderInfo, payCallBack);
     }
 }
--- www/qtpaysdk.js
+++ www/qtpaysdk.js
@@ -1,5 +1,27 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "qtpaysdk", "coolMethod", [arg0]);
+
+/**
+ * This class exposes the QTPaySDK Android SDK functionality to javascript.
+ *
+ * @constructor
+ */
+function QTPaySDK () {}
+
+/**
+ * start order pay by pos
+ * see https://support.qfpay.com/qiantai/Android/OfflineAndroidSDK.html
+ *
+ * @param {Object} orderInfo: all the information qtpaysdk need.
+ * @param {Function} success: callback function on success.
+ * @param {Function} error: callback function on error.
+ */
+QTPaySDK.prototype.orderPayByPos = function(orderInfo, success, error) {
+    exec(success, error, "qtpaysdk", "orderPayByPos", [orderInfo]);
 };
+
+
+/**
+ * Plugin setup boilerplate.
+ */
+module.exports = new QTPaySDK();
